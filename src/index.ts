7fcf34cd3e75c99543ecf8ba323c227b4a8c5976
ryo_sakurai <ryo_sakurai@mates-system.com>
2024-09-12 16:19:01 +0900

    chore: add readonly

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from './image-scanner-with-trivy';
+export * from './types';
--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,8 @@
  * Output configuration for scan logs to CloudWatch Logs.
  */
 export interface CloudWatchLogsOutput {
-  type: 'cloudWatchLogs';
-  logGroupName: string;
+  readonly type: 'cloudWatchLogs';
+  readonly logGroupName: string;
 }
 
 /**
@@ -15,15 +15,15 @@ export type ScanLogsOutputOptions = CloudWatchLogsOutput;
  * Lambda function event object for Scanner Custom Resource.
  */
 export interface ScannerCustomResourceProps {
-  addr: string;
-  imageUri: string;
-  ignoreUnfixed: string;
-  severity: string[];
-  scanners: string[];
-  imageConfigScanners: string[];
-  exitCode: number;
-  exitOnEol: number;
-  trivyIgnore: string[];
-  platform: string;
-  output?: ScanLogsOutputOptions;
+  readonly addr: string;
+  readonly imageUri: string;
+  readonly ignoreUnfixed: string;
+  readonly severity: string[];
+  readonly scanners: string[];
+  readonly imageConfigScanners: string[];
+  readonly exitCode: number;
+  readonly exitOnEol: number;
+  readonly trivyIgnore: string[];
+  readonly platform: string;
+  readonly output?: ScanLogsOutputOptions;
 }
