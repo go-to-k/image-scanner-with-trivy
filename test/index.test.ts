311554bf3ddd91f8d5b4142753ca592ca44b097a
go-to-k <24818752+go-to-k@users.noreply.github.com>
2024-10-27 01:43:09 +0900

    test: improve tests for default log group settings

--- test/index.test.ts
+++ test/index.test.ts
@@ -67,97 +67,111 @@ describe('ImageScannerWithTrivy', () => {
     });
   });
 
-  test('create the default log group with removalPolicy and retentionInDays', () => {
-    template.hasResource('AWS::Logs::LogGroup', {
-      DeletionPolicy: 'Delete',
-      Properties: {
-        LogGroupName: {
-          'Fn::Join': [
-            '',
-            [
-              '/aws/lambda/',
-              {
-                Ref: 'CustomImageScannerWithTrivyCustomResourceLambda470b6343d267f753226c1e99f09f319a884A34E3',
-              },
+  describe('default log group settings', () => {
+    test('create the default log group with removalPolicy and retentionInDays', () => {
+      template.hasResource('AWS::Logs::LogGroup', {
+        DeletionPolicy: 'Delete',
+        Properties: {
+          LogGroupName: {
+            'Fn::Join': [
+              '',
+              [
+                '/aws/lambda/',
+                {
+                  Ref: 'CustomImageScannerWithTrivyCustomResourceLambda470b6343d267f753226c1e99f09f319a884A34E3',
+                },
+              ],
             ],
-          ],
+          },
+          RetentionInDays: 30,
         },
-        RetentionInDays: 30,
-      },
-    });
-  });
-
-  test('only create one log group per singletonFunction and no warns', () => {
-    const app = new App();
-    const stack = new Stack(app, 'TestStack');
-
-    new ImageScannerWithTrivy(stack, 'ImageScannerWithTrivy1', {
-      imageUri: 'imageUri',
-      repository: new Repository(stack, 'ImageRepository1', {}),
-      defaultLogGroupRemovalPolicy: RemovalPolicy.DESTROY,
-      defaultLogGroupRetentionDays: RetentionDays.ONE_MONTH,
-    });
-    new ImageScannerWithTrivy(stack, 'ImageScannerWithTrivy2', {
-      imageUri: 'imageUri',
-      repository: new Repository(stack, 'ImageRepository2', {}),
-      defaultLogGroupRemovalPolicy: RemovalPolicy.DESTROY,
-      defaultLogGroupRetentionDays: RetentionDays.ONE_MONTH,
+      });
     });
 
-    Template.fromStack(stack).resourceCountIs('AWS::Logs::LogGroup', 1);
-    Annotations.fromStack(stack).hasNoWarning(
-      '/TestStack/ImageScannerWithTrivy1',
-      Match.stringLikeRegexp('You have to set the same values for.+'),
-    );
-    Annotations.fromStack(stack).hasNoWarning(
-      '/TestStack/ImageScannerWithTrivy2',
-      Match.stringLikeRegexp('You have to set the same values for.+'),
-    );
-  });
+    test("don't create the default log group if none of the properties for the default log group are specified", () => {
+      const app = new App();
+      const stack = new Stack(app, 'TestStack');
 
-  test('warns when the default log group options are different between ImageScannerWithTrivy constructs', () => {
-    const app = new App();
-    const stack = new Stack(app, 'TestStack');
+      new ImageScannerWithTrivy(stack, 'ImageScannerWithTrivy1', {
+        imageUri: 'imageUri',
+        repository: new Repository(stack, 'ImageRepository1', {}),
+      });
 
-    new ImageScannerWithTrivy(stack, 'ImageScannerWithTrivy', {
-      imageUri: 'imageUri',
-      repository: new Repository(stack, 'ImageRepository1', {}),
-      defaultLogGroupRemovalPolicy: RemovalPolicy.DESTROY,
-      defaultLogGroupRetentionDays: RetentionDays.ONE_MONTH,
-    });
-    new ImageScannerWithTrivy(stack, 'ImageScannerWithTrivyWithDifferentRemovalPolicy', {
-      imageUri: 'imageUri',
-      repository: new Repository(stack, 'ImageRepository2', {}),
-      defaultLogGroupRemovalPolicy: RemovalPolicy.RETAIN,
-      defaultLogGroupRetentionDays: RetentionDays.ONE_MONTH,
-    });
-    new ImageScannerWithTrivy(stack, 'ImageScannerWithTrivyWithDifferentRetentionDays', {
-      imageUri: 'imageUri',
-      repository: new Repository(stack, 'ImageRepository3', {}),
-      defaultLogGroupRemovalPolicy: RemovalPolicy.DESTROY,
-      defaultLogGroupRetentionDays: RetentionDays.ONE_YEAR,
-    });
-    new ImageScannerWithTrivy(stack, 'ImageScannerWithTrivyWithNoDefaultLogGroupOptions', {
-      imageUri: 'imageUri',
-      repository: new Repository(stack, 'ImageRepository4', {}),
+      Template.fromStack(stack).resourceCountIs('AWS::Logs::LogGroup', 0);
     });
 
-    Annotations.fromStack(stack).hasNoWarning(
-      '/TestStack/ImageScannerWithTrivy',
-      Match.stringLikeRegexp('You have to set the same values for.+'),
-    );
-    Annotations.fromStack(stack).hasWarning(
-      '/TestStack/ImageScannerWithTrivyWithDifferentRemovalPolicy',
-      Match.stringLikeRegexp('You have to set the same values for.+'),
-    );
-    Annotations.fromStack(stack).hasWarning(
-      '/TestStack/ImageScannerWithTrivyWithDifferentRetentionDays',
-      Match.stringLikeRegexp('You have to set the same values for.+'),
-    );
-    Annotations.fromStack(stack).hasWarning(
-      '/TestStack/ImageScannerWithTrivyWithNoDefaultLogGroupOptions',
-      Match.stringLikeRegexp('You have to set the same values for.+'),
-    );
+    test('only create one log group per singletonFunction and no warns', () => {
+      const app = new App();
+      const stack = new Stack(app, 'TestStack');
+
+      new ImageScannerWithTrivy(stack, 'ImageScannerWithTrivy1', {
+        imageUri: 'imageUri',
+        repository: new Repository(stack, 'ImageRepository1', {}),
+        defaultLogGroupRemovalPolicy: RemovalPolicy.DESTROY,
+        defaultLogGroupRetentionDays: RetentionDays.ONE_MONTH,
+      });
+      new ImageScannerWithTrivy(stack, 'ImageScannerWithTrivy2', {
+        imageUri: 'imageUri',
+        repository: new Repository(stack, 'ImageRepository2', {}),
+        defaultLogGroupRemovalPolicy: RemovalPolicy.DESTROY,
+        defaultLogGroupRetentionDays: RetentionDays.ONE_MONTH,
+      });
+
+      Template.fromStack(stack).resourceCountIs('AWS::Logs::LogGroup', 1);
+      Annotations.fromStack(stack).hasNoWarning(
+        '/TestStack/ImageScannerWithTrivy1',
+        Match.stringLikeRegexp('You have to set the same values for.+'),
+      );
+      Annotations.fromStack(stack).hasNoWarning(
+        '/TestStack/ImageScannerWithTrivy2',
+        Match.stringLikeRegexp('You have to set the same values for.+'),
+      );
+    });
+
+    test('warns when the default log group options are different between ImageScannerWithTrivy constructs', () => {
+      const app = new App();
+      const stack = new Stack(app, 'TestStack');
+
+      new ImageScannerWithTrivy(stack, 'ImageScannerWithTrivy', {
+        imageUri: 'imageUri',
+        repository: new Repository(stack, 'ImageRepository1', {}),
+        defaultLogGroupRemovalPolicy: RemovalPolicy.DESTROY,
+        defaultLogGroupRetentionDays: RetentionDays.ONE_MONTH,
+      });
+      new ImageScannerWithTrivy(stack, 'ImageScannerWithTrivyWithDifferentRemovalPolicy', {
+        imageUri: 'imageUri',
+        repository: new Repository(stack, 'ImageRepository2', {}),
+        defaultLogGroupRemovalPolicy: RemovalPolicy.RETAIN,
+        defaultLogGroupRetentionDays: RetentionDays.ONE_MONTH,
+      });
+      new ImageScannerWithTrivy(stack, 'ImageScannerWithTrivyWithDifferentRetentionDays', {
+        imageUri: 'imageUri',
+        repository: new Repository(stack, 'ImageRepository3', {}),
+        defaultLogGroupRemovalPolicy: RemovalPolicy.DESTROY,
+        defaultLogGroupRetentionDays: RetentionDays.ONE_YEAR,
+      });
+      new ImageScannerWithTrivy(stack, 'ImageScannerWithTrivyWithNoDefaultLogGroupOptions', {
+        imageUri: 'imageUri',
+        repository: new Repository(stack, 'ImageRepository4', {}),
+      });
+
+      Annotations.fromStack(stack).hasNoWarning(
+        '/TestStack/ImageScannerWithTrivy',
+        Match.stringLikeRegexp('You have to set the same values for.+'),
+      );
+      Annotations.fromStack(stack).hasWarning(
+        '/TestStack/ImageScannerWithTrivyWithDifferentRemovalPolicy',
+        Match.stringLikeRegexp('You have to set the same values for.+'),
+      );
+      Annotations.fromStack(stack).hasWarning(
+        '/TestStack/ImageScannerWithTrivyWithDifferentRetentionDays',
+        Match.stringLikeRegexp('You have to set the same values for.+'),
+      );
+      Annotations.fromStack(stack).hasWarning(
+        '/TestStack/ImageScannerWithTrivyWithNoDefaultLogGroupOptions',
+        Match.stringLikeRegexp('You have to set the same values for.+'),
+      );
+    });
   });
 
   test('throws if memorySize > 10240', () => {
