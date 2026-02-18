c8dda7b51ef22818a65b48728f196c263c1e8ccc
go-to-k <24818752+go-to-k@users.noreply.github.com>
2026-02-18 16:36:53 +0900

    tests

--- test/scan-logs-output.test.ts
+++ test/scan-logs-output.test.ts
@@ -1,8 +1,8 @@
-import { App, RemovalPolicy, Stack } from 'aws-cdk-lib';
+import { App, Stack } from 'aws-cdk-lib';
 import { Match, Template } from 'aws-cdk-lib/assertions';
 import { Repository } from 'aws-cdk-lib/aws-ecr';
-import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
-import { ImageScannerWithTrivyV2, ScanLogsOutput, Scanners, Severity } from '../src';
+import { LogGroup } from 'aws-cdk-lib/aws-logs';
+import { ImageScannerWithTrivyV2, ScanLogsOutput } from '../src';
 
 const getTemplate = (): Template => {
   const app = new App();
@@ -13,18 +13,7 @@ const getTemplate = (): Template => {
   new ImageScannerWithTrivyV2(stack, 'ImageScannerWithTrivyV2', {
     imageUri: 'imageUri',
     repository: repository,
-    ignoreUnfixed: true,
-    severity: [Severity.CRITICAL, Severity.HIGH],
-    scanners: [Scanners.VULN, Scanners.SECRET],
-    exitCode: 1,
-    exitOnEol: 1,
-    trivyIgnore: ['CVE-2023-37920', 'CVE-2019-14697 exp:2023-01-01'],
-    memorySize: 3008,
-    platform: 'linux/arm64',
-    defaultLogGroupRemovalPolicy: RemovalPolicy.DESTROY,
-    defaultLogGroupRetentionDays: RetentionDays.ONE_MONTH,
     scanLogsOutput: ScanLogsOutput.cloudWatchLogs({ logGroup: new LogGroup(stack, 'LogGroup') }),
-    suppressErrorOnRollback: true,
   });
   return Template.fromStack(stack);
 };
